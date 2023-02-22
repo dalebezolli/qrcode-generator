8dbcc9a4692c41c71874b97739d6e9186f9cb935
dalebezolli <deilbezolli@gmail.com>
2023-02-22 13:57:49 +0200

    Convert i, j notation in mask patterns to row, column to conform to dataMatrix functions convention

--- js/mask.mjs
+++ js/mask.mjs
@@ -1,12 +1,12 @@
 const mask = [
-	{mask: 0b000, pattern: (i, j) => (i + j) % 2},
-	{mask: 0b001, pattern: (i, j) => (i) % 2},
-	{mask: 0b010, pattern: (i, j) => (j) % 3},
-	{mask: 0b011, pattern: (i, j) => (i + j) % 3},
-	{mask: 0b100, pattern: (i, j) => (Math.floor(i / 2) + Math.floor(j / 3)) % 2},
-	{mask: 0b101, pattern: (i, j) => (i * j) % 2 + (i * j) % 3},
-	{mask: 0b110, pattern: (i, j) => ((i * j) % 2 + (i * j) % 3) % 2},
-	{mask: 0b111, pattern: (i, j) => ((i + j) % 2 + (i * j) % 3) % 2},
+	{mask: 0b000, pattern: (row, column) => (row + column) % 2},
+	{mask: 0b001, pattern: (row, column) => (row) % 2},
+	{mask: 0b010, pattern: (row, column) => (column) % 3},
+	{mask: 0b011, pattern: (row, column) => (row + column) % 3},
+	{mask: 0b100, pattern: (row, column) => (Math.floor(row / 2) + Math.floor(column / 3)) % 2},
+	{mask: 0b101, pattern: (row, column) => (row * column) % 2 + (row * column) % 3},
+	{mask: 0b110, pattern: (row, column) => ((row * column) % 2 + (row * column) % 3) % 2},
+	{mask: 0b111, pattern: (row, column) => ((row + column) % 2 + (row * column) % 3) % 2},
 ]
 
 export {mask};
